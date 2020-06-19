5d6e97e7c2fa2ae76f25e473801f5e1f6cdc71eb
bnmng <benjamin@bnmng.com>
2020-06-20 00:37:57 +0100

    Created object menu files to reduce redundancy

--- spltcs/urls.py
+++ spltcs/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
 	path('inventory/', include('inventory.urls')),
 	path('entities/', include('entities.urls')),
 	path('maintenance/', include('maintenance.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('login', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+        path('accounts/', include('django.contrib.auth.urls')),
+        path('login', auth_views.LoginView.as_view(), name='login'),
+        path('logout', auth_views.LogoutView.as_view(), name='logout'),
 	path('admin/', admin.site.urls, name='admin'),
 ]
 
